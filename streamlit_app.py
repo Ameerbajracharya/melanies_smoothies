03981bedeb97fc70d511584350919218f53f0684
Ameer Bajracharya <ameerbajracharya36@gmail.com>
2025-07-24 16:37:40 +1000

    API Changed to fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+# smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/all")
 # st.text(smoothiefroot_response.json())
+smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/all")
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
